4d59c443e54d2032c70a17c7592c027e1e243f3c
AnotherSilviu <silviaes1997@gmail.com>
2025-01-08 09:57:18 +0100

    actualizado

--- script-mapa.js
+++ script-mapa.js
@@ -1,26 +1,21 @@
-// Selecciona todos los elementos <path> (provincias)
 const paths = document.querySelectorAll('path');
 const tooltip = document.querySelector('.tooltip');
 
-// Añade eventos a cada provincia
 paths.forEach((path) => {
   path.addEventListener('mouseover', (e) => {
-    // Muestra el tooltip
     const name = e.target.getAttribute('data-name');
-    tooltip.textContent = name; // Muestra el nombre de la provincia
+    tooltip.textContent = name; 
     tooltip.style.display = 'block';
   });
 
   path.addEventListener('mousemove', (e) => {
-    // Posiciona el tooltip justo encima del ratón
-    const x = e.pageX; // Coordenada X del ratón
-    const y = e.pageY; // Coordenada Y del ratón
-    tooltip.style.left = x + 10 + 'px'; // Ajuste horizontal
-    tooltip.style.top = y - 30 + 'px'; // Ajuste vertical (encima del ratón)
+    const x = e.pageX;
+    const y = e.pageY;
+    tooltip.style.left = x + 10 + 'px';
+    tooltip.style.top = y - 30 + 'px';
   });
 
   path.addEventListener('mouseout', () => {
-    // Oculta el tooltip
     tooltip.style.display = 'none';
   });
 });
