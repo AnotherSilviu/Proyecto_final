fcab6fa5b8fddd5006b598b37b2b77c8baa7fb66
sergio-git22 <sergio.rmx22@gmail.com>
2025-01-20 18:12:19 +0100

    esquemaYfotos

--- CLM por Pueblos/pueblos.js
+++ CLM por Pueblos/pueblos.js
@@ -1,13 +1,2 @@
-function verMas() {
 
-  const botonVerMas = document.getElementById("botonVerMas");
-  const contenido = document.getElementById("oculto-alcaraz");
-  if (contenido.style.display === "none") {
-    contenido.style.display = "block";
-    botonVerMas.textContent = "Ver menos";
-  } else {
-    contenido.style.display === "none";
-    botonVerMas.textContent = "Ver más";
-  }
-}
 
