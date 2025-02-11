23dbc3132cf1c101f6eb19d49ab8e0c796e8016d
AnotherSilviu <silviaes1997@gmail.com>
2025-02-11 18:43:42 +0100

    añadido info base de datos

--- Provincias/provincias.js
+++ Provincias/provincias.js
@@ -66,108 +66,108 @@ function pintarDatos(provincia) {
   // Monumentos
   document.getElementById("monuments1_h3").textContent =
     provincia.monuments1_h3 || "Monumento no disponible";
-  document.getElementById("monuments1_img").textContent =
-    provincia.monuments1_h3 || "Monumento no disponible";
+  document.getElementById("monuments1_img").scr =
+    provincia.monuments1_img || "Monumento no disponible";
   document.getElementById("monuments2_h3").textContent =
     provincia.monuments2_h3 || "Monumento no disponible";
-  document.getElementById("monuments2_img").textContent =
-    provincia.monuments1_h3 || "Monumento no disponible";
+  document.getElementById("monuments2_img").scr =
+    provincia.monuments2_img || "Monumento no disponible";
   document.getElementById("monuments3_h3").textContent =
     provincia.monuments3_h3 || "Monumento no disponible";
-  document.getElementById("monuments3_img").textContent =
-    provincia.monuments1_h3 || "Monumento no disponible";
+  document.getElementById("monuments3_img").scr =
+    provincia.monuments3_img || "Monumento no disponible";
 
   // Gastronomía
   document.getElementById("gastronomy_p").textContent =
     provincia.gastronomy_p || "No hay datos de gastronomía.";
-  document.getElementById("gastronomy_1_li_title").textContent =
-    provincia.gastronomy_p || "No hay datos de gastronomía.";
-  document.getElementById("gastronomy_1_li_phrase").textContent =
-    provincia.gastronomy_p || "No hay datos de gastronomía.";
-  document.getElementById("gastronomy_2_li_title").textContent =
-    provincia.gastronomy_p || "No hay datos de gastronomía.";
-  document.getElementById("gastronomy_2_li_phrase").textContent =
-    provincia.gastronomy_p || "No hay datos de gastronomía.";
-  document.getElementById("gastronomy_3_li_title").textContent =
-    provincia.gastronomy_p || "No hay datos de gastronomía.";
-  document.getElementById("gastronomy_3_li_phrase").textContent =
-    provincia.gastronomy_p || "No hay datos de gastronomía.";
+  document.getElementById("gastronomy1_li_title").textContent =
+    provincia.gastronomy1_li_title || "No hay datos de gastronomía.";
+  document.getElementById("gastronomy1_li_phrase").textContent =
+    provincia.gastronomy1_li_phrase || "No hay datos de gastronomía.";
+  document.getElementById("gastronomy2_li_title").textContent =
+    provincia.gastronomy2_li_title || "No hay datos de gastronomía.";
+  document.getElementById("gastronomy2_li_phrase").textContent =
+    provincia.gastronomy2_li_phrase || "No hay datos de gastronomía.";
+  document.getElementById("gastronomy3_li_title").textContent =
+    provincia.gastronomy3_li_title || "No hay datos de gastronomía.";
+  document.getElementById("gastronomy3_li_phrase").textContent =
+    provincia.gastronomy3_li_phrase || "No hay datos de gastronomía.";
 
   // Parques Naturales
   document.getElementById("naturalparks1_h3").textContent =
     provincia.naturalparks1_h3 || "Parque natural no disponible";
   document.getElementById("naturalparks1_p").textContent =
-    provincia.naturalparks1_h3 || "Parque natural no disponible";
+    provincia.naturalparks1_p || "Parque natural no disponible";
   document.getElementById("naturalparks1_img").textContent =
-    provincia.naturalparks1_h3 || "Parque natural no disponible";
+    provincia.naturalparks1_img || "Parque natural no disponible";
   document.getElementById("naturalparks2_h3").textContent =
     provincia.naturalparks2_h3 || "Parque natural no disponible";
   document.getElementById("naturalparks2_p").textContent =
-    provincia.naturalparks2_h3 || "Parque natural no disponible";
+    provincia.naturalparks2_p || "Parque natural no disponible";
   document.getElementById("naturalparks2_img").textContent =
-    provincia.naturalparks2_h3 || "Parque natural no disponible";
+    provincia.naturalparks2_img || "Parque natural no disponible";
 
   // Rutas Turísticas
   document.getElementById("turisticroutes1_h3").textContent =
     provincia.turisticroutes1_h3 || "Ruta turística no disponible";
   document.getElementById("turisticroutes1_p").textContent =
-    provincia.turisticroutes1_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes1_p || "Ruta turística no disponible";
   document.getElementById("turisticroutes1_1").textContent =
-    provincia.turisticroutes1_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes1_1 || "Ruta turística no disponible";
   document.getElementById("turisticroutes1_2").textContent =
-    provincia.turisticroutes1_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes1_2 || "Ruta turística no disponible";
   document.getElementById("turisticroutes1_3").textContent =
-    provincia.turisticroutes1_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes1_3 || "Ruta turística no disponible";
   document.getElementById("turisticroutes1_4").textContent =
-    provincia.turisticroutes1_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes1_4 || "Ruta turística no disponible";
   document.getElementById("turisticroutes2_h3").textContent =
     provincia.turisticroutes2_h3 || "Ruta turística no disponible";
   document.getElementById("turisticroutes2_p").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes2_p || "Ruta turística no disponible";
   document.getElementById("turisticroutes2_1").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes2_1 || "Ruta turística no disponible";
   document.getElementById("turisticroutes2_2").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes2_2 || "Ruta turística no disponible";
   document.getElementById("turisticroutes2_3").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes2_3 || "Ruta turística no disponible";
   document.getElementById("turisticroutes2_4").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes2_4 || "Ruta turística no disponible";
   document.getElementById("turisticroutes3_h3").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes3_h3 || "Ruta turística no disponible";
   document.getElementById("turisticroutes3_p").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes3_p || "Ruta turística no disponible";
   document.getElementById("turisticroutes3_1").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes3_1 || "Ruta turística no disponible";
   document.getElementById("turisticroutes3_2").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes3_2 || "Ruta turística no disponible";
   document.getElementById("turisticroutes3_3").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes3_3 || "Ruta turística no disponible";
   document.getElementById("turisticroutes3_4").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.turisticroutes3_4 || "Ruta turística no disponible";
 
   //Guía Visitantes
   document.getElementById("guide_p").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_p || "Ruta turística no disponible";
   //Sección Clima
   document.getElementById("guide_time_p").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_time_p || "Ruta turística no disponible";
   document.getElementById("guide_time_spring").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_time_spring || "Ruta turística no disponible";
   document.getElementById("guide_time_summer").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_time_summer || "Ruta turística no disponible";
   document.getElementById("guide_time_autum").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_time_autum || "Ruta turística no disponible";
   document.getElementById("guide_time_winter").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_time_winter || "Ruta turística no disponible";
   //Sección Transporte
   document.getElementById("guide_transport_p").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_transport_p || "Ruta turística no disponible";
   document.getElementById("guide_transport_train").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.guide_transport_train || "Ruta turística no disponible";
 
   //Información adicional
   document.getElementById("where_eat").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.where_eat || "Ruta turística no disponible";
   document.getElementById("where_sleep").textContent =
-    provincia.turisticroutes2_h3 || "Ruta turística no disponible";
+    provincia.where_sleep || "Ruta turística no disponible";
 }
