226676df85a2eb0fc9d4208cfc951592f13a7ca8
sergio-git22 <sergio.rmx22@gmail.com>
2025-02-13 13:46:46 +0100

    slider y registro

--- CLM por Pueblos/pueblos.js
+++ CLM por Pueblos/pueblos.js
@@ -1,37 +1,3 @@
-const botonAnterior = document.getElementById("boton-anterior");
-const botonSiguiente = document.getElementById("boton-siguiente");
-const containerCastillos = document.querySelector('.castillos-container');
-const figureCastillos = document.querySelectorAll('.castillos-item');
-
-// Función para mover el slider
-function navegar(direccion) {
-    // Coge el ancho del figure
-    const ancho = containerCastillos[0].offsetWidth;
-
-    // Actualiza el índice en función de la dirección
-    currentIndex += direccion;
-
-    // Ajusta el índice
-    if (currentIndex < 0) {
-        currentIndex = containerCastillos.length - 1;  // Va al último item
-    } else if (currentIndex >= containerCastillos.length) {
-        currentIndex = 0;  // Vuelve al primer item
-    }
-
-    // Mover el contenedor
-    containerCastillos.style.transform = `translateX(-${currentIndex * ancho}px)`;
-}
-
-let currentIndex = 0;
-// Retrasa un item en el slider
-botonAnterior.addEventListener('click', () => {
-    navegar(-1);
-});
-// Avanza item en el slider
-botonSiguiente.addEventListener('click', () => {
-    navegar(1);
-});
-
 // NATURALEZA
 
 const imagenNat = document.querySelectorAll("img-naturaleza");
@@ -45,5 +11,15 @@ imagenNat.forEach((imagen, index) => {
     imagen.addEventListener("mouseleave", () => {
         textoNat[index].style.display = "none"; // Oculta el texto
     });
+});
+
+
+// Evitar scroll hacia el inicio del slider
+const navLinks = document.querySelectorAll('.slider-nav a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (i) => {
+    i.preventDefault();  // Prohíbe el scroll hacia arriba
+  });
 });
 
