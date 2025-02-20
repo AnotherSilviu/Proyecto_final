4508ff3c9ec7fe7bd611ec179b3ce1a2c52fd266
sergio-git22 <sergio.rmx22@gmail.com>
2025-02-20 18:38:35 +0100

    hover active castillos

--- CLM por Pueblos/pueblos.js
+++ CLM por Pueblos/pueblos.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
   sliderNavLinks.forEach((link) => {
     link.addEventListener("click", function (event) {
       event.preventDefault(); // Evita que la página haga scroll hacia arriba
-
+      sliderNavLinks.forEach((navlink) => navlink.classList.remove("active"));
+      this.classList.add("active");
       const targetId = this.getAttribute("href").substring(1); // Obtiene el ID sin "#"
 
       const targetImage = document.getElementById(targetId);
